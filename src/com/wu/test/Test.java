95bd2c622526bf48367798edd74ffc6c8ce94358
13670 <137061115@qq.com>
2020-02-06 15:42:49 +0800

    m的n次方

---
+++ src/com/wu/test/Test.java
@@ -0,0 +1,14 @@
+package com.wu.test;
+
+public class Test {
+    public static void main(String[] args) {
+        int m = 2;
+        int n = 10;
+        int sum1 = 1;
+        for(int i = 1;i <= n;i++){
+            sum1 = sum1*m;
+        }
+        System.out.println(sum1);
+    }
+}
+
